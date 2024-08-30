27fe938b28b0ae63a78c75bc118c29bd9c1cc80e
mithridates <foxhunter_07@yahoo.com>
2024-08-31 00:10:11 +0530

    remove slugify plugin and its dependencies

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,3 @@
 module.exports = ({env}) => ({
-  slugify: {
-    enabled: true,
-    config: {
-      contentTypes: {
-        page: {
-          field: 'slug',
-          references: 'title',
-        },
-        product: {
-          field: 'slug',
-          references: 'title',
-        },
-      },
-    },
-  },
+  
 });
