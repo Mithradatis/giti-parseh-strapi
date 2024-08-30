37846ac85d8ea2e818aa868924d062c6bcc0a23d
mithridates <foxhunter_07@yahoo.com>
2024-08-30 19:30:12 +0530

    add and configure Vercel deploy plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,13 @@
 module.exports = ({env}) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: env('DEPLOY_HOOK'),
+      apiToken: env('GIT_API_TOKEN'),
+      appFilter: env('APP_FILTER'),
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    }
+  },
   slugify: {
     enabled: true,
     config: {
