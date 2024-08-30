e8ec854399a6e76093351b6a5435914590edef57
mithridates <foxhunter_07@yahoo.com>
2024-08-30 23:50:01 +0530

    remove Vercel deploy plugin and its dependencies

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,4 @@
 module.exports = ({env}) => ({
-  "vercel-deploy": {
-    enabled: true,
-    config: {
-      deployHook: env('DEPLOY_HOOK'),
-      apiToken: env('GIT_API_TOKEN'),
-      appFilter: env('APP_FILTER'),
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-    }
-  },
   slugify: {
     enabled: true,
     config: {
