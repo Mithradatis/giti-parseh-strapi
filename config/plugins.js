05bbd69ee369beb0b371a966ec04034e73ce7455
mithridates <foxhunter_07@yahoo.com>
2024-08-31 15:51:16 +0530

    add aws-s3 and related configs

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,40 @@
-module.exports = ({env}) => ({
-
+module.exports = ({ env }) => ({
+    slugify: {
+        enabled: true,
+        config: {
+            contentTypes: {
+                page: {
+                    field: 'slug',
+                    references: 'title',
+                },
+                product: {
+                    field: 'slug',
+                    references: 'title',
+                },
+            },
+        },
+    },
+    upload: {
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            s3Options: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
+              region: env('AWS_REGION'),
+              params: {
+                Bucket: env('AWS_BUCKET_NAME'),
+              },
+            }
+          },
+          actionOptions: {
+            upload: {
+              ACL: null
+            },
+            uploadStream: {
+              ACL: null
+            },
+          }
+        },
+      }
 });
